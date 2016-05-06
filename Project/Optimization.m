6f069991e4e0246d25f15d91616c274dff008b8c
pedrammad <pmadanchian@g.ucla.edu>
2016-05-05 18:14:03 -0700

    Would be the best if we wanted to stretch springs

--- Project/Optimization.m
+++ Project/Optimization.m
@@ -10,7 +10,7 @@ z_constraint = 0.1;              % [m]  width
 y_constraint = 0.1;              % [m]  height
 
 k_spring = 17.38;                % [lbf/in] Spring constant
-g = 32.2*12;                        % [in/s^2]
+g = 32.2*12;                     % [in/s^2]
 rho_ball = 0.283;                % [lbm/in^3] 
 r_ball = 3/8;                    % [in]
 vol_ball = 4/3 * pi *r_ball^3;
@@ -18,7 +18,7 @@ m_ball =rho_ball*vol_ball ;      % [kg]
 
 x = 0.1:0.001:x_constraint;      % [m]
 x = x * 39.3701;                 % [in]
-y = 0.05:0.001:y_constraint;      % [m]
+y = 0.05:0.001:y_constraint;     % [m]
 y = y * 39.3701;                 % [in]
 
 totalCanons = length(y)*length(x);
@@ -28,10 +28,9 @@ iCanon = 0;
 for i = 1:length(x)
     for j = 1:length(y)
         iCanon = iCanon + 1;
+        
         canon (iCanon, 1) = x(i);
         canon (iCanon, 2) = y(j);
-        
-
     end
 end
 
@@ -41,7 +40,7 @@ for k = 1:iCanon
    
    alpha(k) = atan(y(k)./x(k));        %[rad] found based on x and y
    L(k) = x(k)./ cos(alpha(k));        %[in] Canon Length
-   v_top(k) = sqrt (2*g*L(k).*sin(alpha(k)) + k_spring/m_ball*(L(k)-2).^2); %[in/s]
+   v_top(k) = sqrt ( k_spring/m_ball*(L(k)-2).^2 - 2*g*L(k).*sin(alpha(k))); %[in/s]
    
    %[in] Range Equation
    d(k) = v_top(k).*cos(alpha(k))./g *(v_top(k).*sin(alpha(k)) + ...
@@ -51,7 +50,7 @@ end
 
 [maxValue,maxIndex] = max(d);
 
-fprintf (['Maximum Range of ' num2str(d(maxIndex)/39.3701) ' inches occurs @ Case ' ...
+fprintf (['Maximum Range of ' num2str(d(maxIndex)/39.3701) ' meters occurs @ Case ' ...
                   num2str(maxIndex) ' When: \n Length  =\t' ...
                   num2str(x(maxIndex)/39.3701) ' meters & \n Height =\t' ...
                   num2str(y(maxIndex)/39.3701) ' meters & \n Alpha =\t' ...
